b7b853d411fdef827059c42083832d4fb4995bb1
ralexale <ralexale@gmail.com>
2023-03-26 21:07:32 -0500

    add animations and cartel

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,15 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { loadingReducer } from "../reducers/loadingReducer";
-// import { userReducer } from "../reducers/userReducer";
 
 const initialState = [];
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case "addBurger":
-            /** Se agregan nuevos elementos al arreglo
-             * OJO: la acción debe contener una propiedad 'price'
-             * que contiene información
-             */
             const newState = state.concat([action.price]);
             return newState;
         case "reset":
